aff83756c1638f18bfff26864a4c460320bd1167
Khalid Jameer <41968688+kjameer0@users.noreply.github.com>
2024-09-22 15:27:48 -0400

    feat: completed MVP

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/smtp"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -16,44 +17,15 @@ import (
 	// Import godotenv
 )
 
-func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-	// // smtp server configuration.
-	subject := "Subject: check out my notes!"
-	message := "This is the email body."
-	emailConfig := EmailConfig{
-		to:       []string{os.Getenv("EMAIL_RECEIVER")},
-		from:     os.Getenv("EMAIL_SENDER"),
-		password: os.Getenv("EMAIL_PASSWORD"),
-		smtpHost: "smtp.gmail.com",
-		smtpPort: "587",
-	}
-	sendEmail(emailConfig, subject, message)
-	notesPath := os.Getenv("NOTES_PATH")
-	apiKey := os.Getenv("API_KEY")
-	client := claude.NewClient(apiKey)
-	notes := readLastWeekNotes(notesPath)
-	summaries := []string{}
-	var wg = &sync.WaitGroup{}
-
-	ch := make(chan string, len(notes))
-	for _, note := range notes {
-		wg.Add(1)
-		go func() {
-			ch <- summarizeNote(client, note[1], note[0])
-			wg.Done()
-		}()
-		summary := <-ch
-		summaries = append(summaries, summary)
-	}
-	wg.Wait()
-	fmt.Println(summaries)
-
+type Summary struct {
+	Day     int
+	Content string
+	Err     error
 }
-
+type Note struct {
+	Day     int
+	Content string
+}
 type EmailConfig struct {
 	to       []string
 	from     string
@@ -62,12 +34,59 @@ type EmailConfig struct {
 	smtpPort string
 }
 
-func sendEmail(emailConfig EmailConfig, subject string, message string) {
+const timePeriod = 7
 
-	// // Message.
-	// // Authentication.
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	// // smtp server configuration.
+	subject := "Subject: check out my notes!"
+	emailConfig := EmailConfig{
+		to:       []string{os.Getenv("EMAIL_RECEIVER")},
+		from:     os.Getenv("EMAIL_SENDER"),
+		password: os.Getenv("EMAIL_PASSWORD"),
+		smtpHost: "smtp.gmail.com",
+		smtpPort: "587",
+	}
+	notesPath := os.Getenv("NOTES_PATH")
+	apiKey := os.Getenv("API_KEY")
+	client := claude.NewClient(apiKey)
+	notes := readLastWeekNotes(notesPath)
+	var wg = &sync.WaitGroup{}
+
+	ch := make(chan Summary, len(notes))
+	wg.Add(len(notes))
+	for i, note := range notes {
+		go func(g int, note Note) {
+			defer wg.Done()
+			summary, summaryErr := summarizeNote(client, note.Content)
+			if summaryErr != nil {
+				summary = "Summarization failed"
+				writeToLogFile(summaryErr.Error())
+				sendEmail(emailConfig, "Summarization failed", "Something went wrong while summarizing your notes. Please check the logs for more details.")
+			}
+			summaryStruct := Summary{
+				Day:     g,
+				Content: summary,
+				Err:     summaryErr,
+			}
+			ch <- summaryStruct
+		}(i, note)
+	}
+	wg.Wait()
+	close(ch)
+	summaries := [timePeriod]string{}
+	for summary := range ch {
+		dayOfWeek := time.Weekday(summary.Day).String()
+		summaries[summary.Day] = dayOfWeek + ": \n" + summary.Content
+	}
+	sendEmail(emailConfig, subject, strings.Join(summaries[:], "\n"))
+}
+
+func sendEmail(emailConfig EmailConfig, subject string, message string) {
 	auth := smtp.PlainAuth("", emailConfig.from, emailConfig.password, emailConfig.smtpHost)
-	// // Sending email.
 	fullEmail := []byte(fmt.Sprintf("%v\r\n\r\n%v\r\n", subject, message))
 	err := smtp.SendMail(emailConfig.smtpHost+":"+emailConfig.smtpPort, auth, emailConfig.from, emailConfig.to, fullEmail)
 	if err != nil {
@@ -75,14 +94,13 @@ func sendEmail(emailConfig EmailConfig, subject string, message string) {
 		log.Fatal(err)
 		return
 	}
-	//write to log file
 	writeToLogFile("Email Sent Successfully!")
 	fmt.Println("Email Sent Successfully!")
 }
 
 // request a summary of the note from the client
-func summarizeNote(client *claude.Client, content string, dayOfWeek string) string {
-	prompt := "Summarize the text after this colon in about 3-4 sentences. If a summary cannot be made just say 'No summary available'."
+func summarizeNote(client *claude.Client, content string) (string, error) {
+	prompt := "Summarize the text after this colon in about 4 sentences. Do not reference that you are making a summary. If a summary cannot be made just say 'No summary available'. If a summary can be made, make sure you summarize every bullet point"
 	m := claude.RequestBodyMessages{
 		Model:     "claude-3-5-sonnet-20240620",
 		MaxTokens: 400,
@@ -97,17 +115,16 @@ func summarizeNote(client *claude.Client, content string, dayOfWeek string) stri
 	res, err := client.CreateMessages(ctx, m)
 	if err != nil {
 		fmt.Println(err)
-		return dayOfWeek + ": No summary available"
+		writeToLogFile(err.Error())
+		return "No summary available", err
 	}
-	return dayOfWeek + ": " + (res.Content[0].Text)
+	return (res.Content[0].Text), nil
 }
-func readLastWeekNotes(notesPath string) [][2]string {
+func readLastWeekNotes(notesPath string) []Note {
 	localTime := time.Now()
-	daysInWeek := 7
-	//go through each file from the past 7 days every sunday
-	//make a and return that slice, which contains the notes from every day in the week
-	notes := [][2]string{}
-	for daysFromToday := daysInWeek; daysFromToday > 0; daysFromToday -= 1 {
+	//go through each file from the specified time period
+	notes := []Note{}
+	for daysFromToday := timePeriod; daysFromToday > 0; daysFromToday -= 1 {
 		day := localTime.AddDate(0, 0, -daysFromToday)
 		curFileName := (convertDateToFilePath(day))
 		filePath := filepath.Clean(notesPath) + string(filepath.Separator) + curFileName + ".md"
@@ -120,7 +137,7 @@ func readLastWeekNotes(notesPath string) [][2]string {
 				log.Fatal("Failed to open file specified by path " + filePath)
 			}
 		}
-		notes = append(notes, [2]string{day.Weekday().String(), string(textBytes)})
+		notes = append(notes, Note{int(day.Weekday()), string(textBytes)})
 	}
 	return notes
 }
